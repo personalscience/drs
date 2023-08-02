f989eb81b78423b1e070e1017b6097090f294ed7
Richard Sprague <richardsprague@gmail.com>
2023-08-01 17:53:03 -0700

    basic hello world in next.js

---
+++ frontend/drr_frontend/pages/hello.tsx
@@ -0,0 +1,12 @@
+// pages/hello.tsx
+import React from 'react';
+
+const HelloPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default HelloPage;
